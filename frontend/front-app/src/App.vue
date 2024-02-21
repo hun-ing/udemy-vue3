b8d1391887a4e3c39c58309ae4f40c9e2cfbc2ea
yonghun.jeong <younghun.jeong@rapeech.com>
2024-02-21 10:34:28 +0900

    feat: 110 강의 시작

--- frontend/front-app/src/App.vue
+++ frontend/front-app/src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <section>
-  </section>
+  <div>
+    <TheHeader></TheHeader>
+    <BadgeList></BadgeList>
+    <UserInfo
+        :full-name="activeUser.name"
+        :info-text="activeUser.description"
+        :role="activeUser.role"
+    ></UserInfo>
+  </div>
 </template>
 
 <script setup>
+import TheHeader from "@/components/TheHeader.vue";
+import BadgeList from "@/components/BadgeList.vue";
+import UserInfo from "@/components/UserInfo.vue";
+import {ref} from "vue";
+
+const activeUser = ref({
+  name: 'Maximilian Schwarzmüller',
+  description: 'Site owner and admin',
+  role: 'admin',
+});
 </script>
 
 <style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
