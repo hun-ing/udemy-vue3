dcaa875c2586c26156967e2f15bd237b1bd12e07
yonghun.jeong <younghun.jeong@rapeech.com>
2024-02-22 08:40:06 +0900

    feat: 128~ 강의
    - 초기 설정
    - LearningResource.vue 컴포넌트 생성 및 사용

--- frontend/front-app/src/App.vue
+++ frontend/front-app/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <section>
-  </section>
+  <ul>
+    <LearningResource v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.description" :link="res.link" />
+  </ul>
 </template>
 
 <script setup>
+import {ref} from "vue";
+import LearningResource from "@/components/learning-resources/LearningResource.vue";
+
+const storedResources = ref([
+  { id: 'test1', title: 'Test1', description: 'test111', link: 'https://vuejs.org'},
+  { id: 'test2', title: 'Test2', description: 'test222', link: 'https://google.org'},
+]);
 </script>
 
 <style>
---
+++ frontend/front-app/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,22 @@
+<template>
+  <li >
+    <div>
+      <header>
+        <h3>{{title}}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{description}}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script setup>
+defineProps(['title', 'description', 'link']);
+</script>
+
+<style scoped>
+
+</style>
