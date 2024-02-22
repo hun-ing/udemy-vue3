0d1908201a6843e2cbb922df2bb28a6712eb658c
yonghun.jeong <younghun.jeong@rapeech.com>
2024-02-22 15:33:52 +0900

    feat: 152~ 강의
    - 초기 설정 vue2 -> vue3 문법으로 변경

--- frontend/front-app/src/App.vue
+++ frontend/front-app/src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <section>
-  </section>
+  <LearningSurvey @survey-submit="storeSurvey"></LearningSurvey>
+  <UserExperiences :results="savedSurveyResults"></UserExperiences>
 </template>
 
 <script setup>
+import LearningSurvey from "@/components/survey/LearningSurvey.vue";
+import UserExperiences from "@/components/survey/UserExperiences.vue";
+import {ref} from "vue";
+
+const savedSurveyResults = ref([]);
+const storeSurvey = (surveyData) => {
+  const surveyResult = {
+    name: surveyData.userName,
+    rating: surveyData.rating,
+    id: new Date().toISOString(),
+  };
+  savedSurveyResults.value.push(surveyResult);
+  console.log(surveyResult);
+}
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
---
+++ frontend/front-app/src/components/survey/UserExperiences.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <BaseCard>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <BaseButton>Load Submitted Experiences</BaseButton>
+      </div>
+      <ul>
+        <SurveyResult
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></SurveyResult>
+      </ul>
+    </BaseCard>
+  </section>
+</template>
+
+<script setup>
+import SurveyResult from "@/components/survey/SurveyResult.vue";
+
+defineProps(['results']);
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
