827be42c59a1cfdccc45b62c8b2e471af6a026d8
yonghun.jeong <younghun.jeong@rapeech.com>
2024-02-22 14:40:19 +0900

    feat: 142~149 강의
    - v-model을 통한 form 데이터 제어
    - v-model을 통한 자식 컴포넌트와의 통신
    - 조건에 따른 클래스 추가

--- frontend/front-app/src/App.vue
+++ frontend/front-app/src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <section>
-  </section>
+  <TheForm></TheForm>
 </template>
 
 <script setup>
+import TheForm from '@/components/TheForm.vue';
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
 </style>
---
+++ frontend/front-app/src/components/RatingControl.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul>
+    <li :class="{active: modelValue === 'poor'}" @click="activate('poor')"><button type="button">Poor</button></li>
+    <li :class="{active: modelValue === 'average'}" @click="activate('average')"><button type="button">Average</button></li>
+    <li :class="{active: modelValue === 'great'}" @click="activate('great')"><button type="button">Great</button></li>
+  </ul>
+</template>
+
+<script setup>
+defineProps(['modelValue']);
+const emits = defineEmits(['update:modelValue']);
+
+const activate = (option) => {
+  emits('update:modelValue', option);
+}
+</script>
+
+<style scoped>
+.active {
+  border-color: #a00078;
+}
+
+.active button {
+  color: #a00078;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
