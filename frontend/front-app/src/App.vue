532651f7ec891012756a3ea3f0c59e00e2e23d6f
yonghun.jeong <younghun.jeong@rapeech.com>
2024-02-22 09:18:34 +0900

    feat: 128~ 강의
    - 공통 UI BaseCard.vue, BaseButton.vue 컴포넌트 생성 및 사용
    - 레이아웃 TheHeader.vue 컴포넌트 생성 및 사용

--- frontend/front-app/src/App.vue
+++ frontend/front-app/src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <ul>
-    <LearningResource v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.description" :link="res.link" />
-  </ul>
+  <TheHeader title="RememberMe"/>
+  <StoredResources :resources/>
 </template>
 
 <script setup>
 import {ref} from "vue";
-import LearningResource from "@/components/learning-resources/LearningResource.vue";
+import StoredResources from "@/components/learning-resources/StoredResources.vue";
+import TheHeader from "@/components/layouts/TheHeader.vue";
 
-const storedResources = ref([
-  { id: 'test1', title: 'Test1', description: 'test111', link: 'https://vuejs.org'},
-  { id: 'test2', title: 'Test2', description: 'test222', link: 'https://google.org'},
+const resources = ref([
+  {id: 'test1', title: 'Test1', description: 'test111', link: 'https://vuejs.org'},
+  {id: 'test2', title: 'Test2', description: 'test222', link: 'https://google.org'},
 ]);
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
---
+++ frontend/front-app/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <LearningResource v-for="res in resources" :key="res.id" :title="res.title" :description="res.description" :link="res.link" />
+  </ul>
+</template>
+
+<script setup>
+import LearningResource from "@/components/learning-resources/LearningResource.vue";
+
+defineProps(['resources']);
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
