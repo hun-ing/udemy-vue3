fc7c35869849a692d40ca5b9a579b025b50a84a1
yonghun.jeong <younghun.jeong@rapeech.com>
2024-02-28 16:00:25 +0900

    feat: 213 ~ 강의
    - 전역 상태 관리 pinia 추가 및 간단한 사용

--- frontend/front-app/src/main.js
+++ frontend/front-app/src/main.js
@@ -1,30 +1,15 @@
 import App from './App.vue';
 import {createApp} from 'vue'
-import {createPinia} from 'pinia'
-import BaseModal from "@/components/BaseModal.vue";
-import {createRouter, createWebHistory} from "vue-router";
-import AllUsers from "@/pages/AllUsers.vue";
-import CourseGoals from "@/pages/CourseGoals.vue";
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/', component: AllUsers
-        },
-        {
-            path: '/goals', component: CourseGoals
-        },
-    ]
-});
+import {createPinia, defineStore} from 'pinia'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router);
-
-app.component('BaseModal', BaseModal);
-
-router.isReady().then(() => {
-    app.mount('#app')
+export const useCreateStore = defineStore('createStore', {
+    state: () => ({
+        counter: 0
+    }),
 })
+
+app.use(createPinia())
+
+app.mount('#app')
