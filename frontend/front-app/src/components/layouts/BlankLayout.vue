1a2a5029c9ac81710848bfdc7da07e068a58798b
yonghun.jeong <jeong.yonghun90@gmail.com>
2024-03-19 20:35:28 +0900

    feat: 다중 레이아웃
    - 로그인 -> BlankLayout.vue 적용
    - 로그인 제외 -> DefaultLayout.vue 적용

---
+++ frontend/front-app/src/components/layouts/BlankLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-main>
+    <v-container
+      fluid
+    >
+      <router-view v-slot="slotProps">
+        <transition
+          name="route"
+          mode="out-in"
+        >
+          <component :is="slotProps.Component" />
+        </transition>
+      </router-view>
+    </v-container>
+  </v-main>
+</template>
+
+<script setup>
+</script>
---
+++ frontend/front-app/src/components/layouts/DefaultLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <TheNav />
+  <TheHeader />
+
+  <v-main>
+    <v-container
+      fluid
+    >
+      <router-view v-slot="slotProps">
+        <transition
+          name="route"
+          mode="out-in"
+        >
+          <component :is="slotProps.Component" />
+        </transition>
+      </router-view>
+    </v-container>
+  </v-main>
+</template>
+
+
+<script setup>
+import TheNav from '@/components/layouts/TheNav.vue';
+import TheHeader from '@/components/layouts/TheHeader.vue';
+</script>
